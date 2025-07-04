94f5cc98517b3b93ea01e522dc0ff98e39c0c747
Jennie Andersson <jennie.andersson@vinnovera.se>
2025-07-04 17:43:06 +0200

    feat: create empty site

--- src/app/page.tsx
+++ src/app/page.tsx
@@ -1,23 +1,8 @@
-'use client'
-
-import { UserForm } from './components/UserForm'
-
 export default function Home() {
-  const handleSubmit = async (event: React.FormEvent<HTMLFormElement>) => {
-    event.preventDefault()
-    const formData = new FormData(event.currentTarget)
-    await fetch('/api/form', {
-      method: 'POST',
-      body: formData,
-    })
-  }
-
   return (
     <div className='grid grid-rows-[20px_1fr_20px] items-center justify-items-center min-h-screen p-8 pb-20 gap-16 sm:p-20 font-[family-name:var(--font-geist-sans)]'>
       <main className='flex flex-col gap-[32px] row-start-2 items-center sm:items-start'>
-        <h1 className='text-[32px] font-bold text-gray-900'>Forms</h1>
-        <UserForm title='Create a user' onSubmit={handleSubmit} />
-        <UserForm title='Create a user with action' />
+        <h1>Fullstack Course</h1>
       </main>
     </div>
   )
